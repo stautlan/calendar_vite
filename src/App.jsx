3e6dc14c98ee679b3df2c79bb990a626758a47da
m.kurochkin <stautlan@yandex.ru>
2023-06-30 14:56:25 +0300

    add calendar

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import CalendarSimple from './components/CalendarSimple'
+import CalendarStyling from './components/CalendarStyling'
+import CalendarNew from './components/CalendarNew'
+
+const args = 'prev';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <CalendarSimple prop={args} />
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <CalendarStyling />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <CalendarNew />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
